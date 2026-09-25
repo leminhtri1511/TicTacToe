88089a821d9fd0f509dbb4f1bb9567314e2b8c1d
leminhtri1511 <leminhtri151102@gmail.com>
2026-09-25 11:58:57 +0700

    style: ui styling

---
+++ Assets/TTT/Scripts/Utils/GameState.cs
@@ -0,0 +1,10 @@
+﻿namespace TTT.Scripts.Utils
+{
+    public enum GameState
+    {
+        Playing,
+        XWin,
+        OWin,
+        Draw
+    }
+}
--- Assets/TTT/Scripts/View/UICell.cs
+++ Assets/TTT/Scripts/View/UICell.cs
@@ -1,6 +1,6 @@
 using System;
 using TMPro;
-using TTT.Scripts.Manager;
+using TTT.Scripts.Utils;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -10,11 +10,16 @@ namespace TTT.Scripts.View
     {
         [Header("UI")]
         [SerializeField] private Button _button;
+        [SerializeField] private RectTransform _overlayRectTransform;
+        [SerializeField] private Image _outline;
         [SerializeField] private TextMeshProUGUI _text;
+
+        [Header("Configs")]
+        [SerializeField] private Color32 _xColor = new(255, 255, 255, a: 255);
+        [SerializeField] private Color32 _oColor = new(255, 255, 255, a: 255);
 
         private int _row;
         private int _column;
-
         private Action<int, int> _onClicked;
 
         public void Initialize(int row, int column, Action<int, int> onClicked)
@@ -26,20 +31,37 @@ namespace TTT.Scripts.View
             _button.onClick.RemoveAllListeners();
             _button.onClick.AddListener(OnButtonClicked);
 
-            SetState(CellState.Empty);
+            SetCellState(CellIdentity.Empty);
         }
 
-        public void SetState(CellState state)
+        public void SetCellState(CellIdentity cellIdentity)
         {
-            _text.text = state switch
-            {
-                CellState.Empty => "",
-                CellState.X => "X",
-                CellState.O => "O",
-                _ => _text.text
-            };
+            _outline.gameObject.SetActive(true);
 
-            _button.interactable = state == CellState.Empty;
+            switch (cellIdentity)
+            {
+                case CellIdentity.X:
+                    _text.text = "X";
+                    SetColor(_xColor);
+                    break;
+                case CellIdentity.O:
+                    _text.text = "O";
+                    SetColor(_oColor);
+                    break;
+                case CellIdentity.Empty:
+                    _text.text = "";
+                    _outline.gameObject.SetActive(false);
+                    ToggleOverlayRT(false);
+                    break;
+            }
+
+            SetInteractable(cellIdentity == CellIdentity.Empty);
+        }
+
+        private void SetColor(Color32 color)
+        {
+            _outline.color = color;
+            _text.color = color;
         }
 
         public void SetInteractable(bool interactable)
@@ -50,6 +72,11 @@ namespace TTT.Scripts.View
         private void OnButtonClicked()
         {
             _onClicked?.Invoke(_row, _column);
+        }
+
+        public void ToggleOverlayRT(bool isVisible)
+        {
+            _overlayRectTransform.gameObject.SetActive(isVisible);
         }
     }
 }
